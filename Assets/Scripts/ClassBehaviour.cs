b61fdf44f4e591564f74a1bb2a3236a60f2538c4
Yilmaz Tarik Ipekci <ytarik.ipekci@gmail.com>
2023-07-08 21:50:04 +0300

    One bug is fixed about class selecting.

--- Assets/Scripts/ClassBehaviour.cs
+++ Assets/Scripts/ClassBehaviour.cs
@@ -7,18 +7,24 @@ public class ClassBehaviour : MonoBehaviour
     public void SelectArcher()
     {
         isArcher = true;
+        isMage = false;
+        isKnight = false;
         StartTheGame();
     }
 
     public void SelectMage()
     {
         isMage = true;
+        isArcher = false;
+        isKnight = false;
         StartTheGame();
     }
 
     public void SelectKnight()
     {
         isKnight = true;
+        isArcher = false;
+        isMage = false;
         StartTheGame();
     }
 
